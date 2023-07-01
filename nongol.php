247415824f1faa8433002fd939188da1a18eb6f3
Agung Sundoro <muhagung2001@gmail.com>
2023-07-01 14:00:40 +0700

    feat: finish creating simple modal

--- nongol.php
+++ nongol.php
@@ -14,3 +14,16 @@
  * Text Domain:       my-nongol-plugin
  * Domain Path:       /languages
  */
+
+// Enqueue css and js for the plugin.
+function nongol_scripts(){
+    wp_enqueue_style('nongol-style', plugins_url('assets/css/style.css', __FILE__));
+    wp_enqueue_script('nongol-script', plugins_url('assets/js/script.js', __FILE__), array('jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'nongol_scripts');
+
+// Modal DOM.
+function modal_dom(){
+    include( plugin_dir_path( __FILE__ ) . 'views/modal.php');
+}
+add_action('wp_footer', 'modal_dom');
