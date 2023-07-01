b81142ad907ebe54b59a5f0956bd8454d87d572d
Agung Sundoro <muhagung2001@gmail.com>
2023-07-01 13:52:54 +0700

    chore: initial commit add directories and files

---
+++ nongol.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ * Plugin Name:       Nongol
+ * Plugin URI:        https://example.com/plugins/the-basics/
+ * Description:       Simple modal and popup plugin.
+ * Version:           0.0.1
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Agung Sundoro
+ * Author URI:        https://agung2001.github.io
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       my-nongol-plugin
+ * Domain Path:       /languages
+ */
